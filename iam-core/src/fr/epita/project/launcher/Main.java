cd5bc42beb11cc4c434f34a8bfa678febca09227
capsosk <jakubnovak97@gmail.com>
2018-02-18 19:50:39 +0100

    main finished

--- iam-core/src/fr/epita/project/launcher/Main.java
+++ iam-core/src/fr/epita/project/launcher/Main.java
@@ -1,5 +1,6 @@
 package fr.epita.project.launcher;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
@@ -22,10 +23,14 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		//id1.printIdentity();
 		UserJDBCAO userDB = new UserJDBCAO();
+		IdentityJDBCDAO idDB = new IdentityJDBCDAO();
 		while (userDB.Login(scanner) != true) {
 			userDB.Login(scanner);
 		}
-		scanner.close();
+		int result = select(scanner);
+		
+		userORid(scanner, userDB, idDB, result);
+		
 		/*
 		IdentityJDBCDAO dataB = new IdentityJDBCDAO();
 		//dataB.delete(id1);
@@ -39,6 +44,82 @@ public class Main {
 		//user1.createUser(u1);
 		user1.printDB();
 		//System.out.println(u1.getPwdHash());*/
+		scanner.close();
+	}
+	private static int select(Scanner scanner) {
+		boolean bool = true;
+		int result = 0;
+		while (bool == true) {
+			System.out.println("What do you want to do? Edit Users (1) or Identities(2)? ");
+			System.out.println("Write down your number");
+			result = scanner.nextInt();
+			if (result == 1) {
+				System.out.println("Users");
+				bool = false;
+			}else if(result == 2) {System.out.println("Identities"); bool = false;}
+			else { System.out.println("only accepting 1 or 2");}
+		}
+		return result;
+	}
+	
+	private static void userORid(Scanner scanner, UserJDBCAO userDB, IdentityJDBCDAO idDB,int result)
+			throws FileNotFoundException, IOException, DaoCreationException, ClassNotFoundException, SQLException {
+		int again = 0;
+		boolean bool = true;
+		if (result == 1) {
+			while (bool == true) {
+				System.out.println("What do you want to do with Users? Update(1), Delete(2) or Create(3)?");
+				System.out.println("you can also print all users with (4");
+				result = scanner.nextInt();
+				
+				switch (result) {
+	            case 1:  bool = false;userDB.update();
+	                     break;
+	            case 2:  bool = false;userDB.delete();
+	                     break;
+	            case 3:  bool = false;userDB.createUser();
+	                     break;
+	            case 4:  bool = false;userDB.printDB();
+                		 break;
+	            default: System.out.println("only accepting 1,2,3,4");
+	                     break;
+	            }
+				
+			}
+			
+		}
+			
+		else if (result == 2) {
+			while (bool == true) {
+				System.out.println("What do you want to do with Identities? Update(1), Delete(2), Create(3) or Search?");
+				System.out.println("you can also print all identities with (5)");
+				result = scanner.nextInt();
+				
+				switch (result) {
+	            case 1:  bool = false;idDB.update();
+	                     break;
+	            case 2:  bool = false;idDB.delete();
+	                     break;
+	            case 3:  bool = false;idDB.create();
+	                     break;
+	            case 5:  bool = false;idDB.printDB();
+                		 break;
+	            case 4:  bool = false;idDB.search();
+       		 			 break;
+	            default: System.out.println("only accepting 1,2,3,4,");
+	                     break;
+	            }
+			}
+		}else {return;}
+		
+		System.out.println("What do you want to edit now? Users(1), Identities(2) or write any other number to exit");
+		again = scanner.nextInt();
+		userORid(scanner, userDB, idDB, again);
+		
+		
+			
+			
+		
 	}
 
 }
